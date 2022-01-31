6119f5555eff834038ae599d3300d54b848a179b
NEETAZ11 <94694893+NEETAZ11@users.noreply.github.com>
2022-01-31 22:52:47 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+
+  (function ($) {
+  
+  "use strict";
+
+
+    // REVIEWS CAROUSEL
+    $('.reviews-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 300,
+        smartSpeed: 500,
+        responsive:{
+          0:{
+            items:1,
+          },
+          768:{
+            items:2,
+            margin: 100,
+          },
+          1280:{
+            items:2,
+            margin: 100,
+          }
+        }
+    });
+  })(window.jQuery);
+ 
+
+   
+
